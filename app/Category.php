443ecaec23ccaa0420a1fe02c904a484395219d4
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-05-11 11:59:35 +0300

    Create category model

---
+++ app/Category.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Category
+ *
+ * @package App
+ *
+ * @property int $id
+ * @property string $title
+ * @property-read Carbon $created_at
+ * @property-read Carbon $updated_at
+ */
+class Category extends Model
+{
+    //
+}
