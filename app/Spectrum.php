85d635583ee094648e739cf98b08398d864c5b87
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-05-11 17:34:57 +0300

    update(Spectrum model): add phpdoc and user relation

--- app/Spectrum.php
+++ app/Spectrum.php
@@ -2,15 +2,49 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * Class Spectrum
+ *
+ * @package App
+ *
+ * @property int $id
+ * @property-read int $user_id
+ * @property-read int $category_id
+ * @property string $title
+ * @property string $system
+ * @property string $mode
+ * @property int $temp
+ * @property string $state
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read User $user
+ * @property-read Category $category
+ */
 class Spectrum extends Model
 {
     /**
+     * The table associated with the model
+     *
+     * @var string
+     */
+    protected $table = 'spectra';
+
+    /**
+     * @return BelongsTo|User
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
      * @return BelongsTo|Category
      */
-    public function category()
+    public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
     }
