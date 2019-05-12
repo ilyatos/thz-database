04c2e9484ac85c51c73404ff5ff2fc3c813dc041
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-05-12 15:50:02 +0300

    update(welcome.blade): place login/reg links at page center

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,27 +65,14 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Explore</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     Welcome to THz modern storage
                 </div>
-
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="{{ route('login') }}">Login</a>
+                    <span>|</span>
+                    <a href="{{ route('register') }}">Register</a>
                 </div>
             </div>
         </div>
