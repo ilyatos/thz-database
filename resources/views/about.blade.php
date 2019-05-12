47b5cd4536fe75d6cb6d48c9b519049bb81d4378
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-05-12 18:49:23 +0300

    create(about.blade)

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-start">
+        <div class="col-10">
+                <div class="card">
+                    <div class="card-body">
+                        Spectrum data + plot
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
+@endsection
