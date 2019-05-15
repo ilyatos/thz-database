462f4eea56201c7bb23485abfe126d1b9ce8c8d6
Ilya Goryachev <ilyatos.gor@gmail.com>
2019-05-15 22:58:48 +0300

    update(filesystems.php, storage .gitignore): add 'spectra' storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'visibility' => 'public',
         ],
 
+        'spectra' => [
+            'driver' => 'local',
+            'root' => storage_path('app/spectra'),
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
