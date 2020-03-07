e9085b35ac9e1bd61a65f70e5889573075b5aca5
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-03-07 19:18:55 +0300

    Add Experiment relations

--- thz-ms/app/Models/Experiment.php
+++ thz-ms/app/Models/Experiment.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Experiment extends Model
 {
-    //
+    /**
+     * @return BelongsTo|Research
+     */
+    public function research(): BelongsTo
+    {
+        return $this->belongsTo(Research::class);
+    }
+
+    /**
+     * @return BelongsTo|User
+     */
+    public function populator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'populator_id');
+    }
+
+    /**
+     * @return BelongsTo|System
+     */
+    public function system(): BelongsTo
+    {
+        return $this->belongsTo(System::class);
+    }
 }
