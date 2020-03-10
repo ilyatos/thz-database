1f8177de4b66087eae1d1dd7b50856ae2d17d459
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-03-10 23:54:49 +0300

    Refactor controllers structure

--- thz-ms/app/Http/Controllers/SpectrumController.php
+++ thz-ms/app/Http/Controllers/SpectrumController.php
@@ -2,104 +2,104 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Spectrum\StoreRequest;
-use App\Models\Category;
-use App\Models\Spectrum;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\View\View;
-
+/**
+ * Class SpectrumController
+ *
+ * @package App\Http\Controllers
+ *
+ * @deprecated
+ */
 class SpectrumController extends Controller
 {
-    /** @var array Spectrum model fields */
-    private const SPECTRUM_FIELDS = [
-        'system',
-        'mode',
-        'category_id',
-        'title',
-        'temp',
-        'state',
-    ];
-
-    /**
-     * Show the form for creating a new resource
-     *
-     * @return View
-     */
-    public function create(): View
-    {
-        return view('spectrum.create', [
-            'categories' => Category::orderBy('title')->get(),
-        ]);
-    }
-
-    /**
-     * Store a newly created resource in storage
-     *
-     * @param StoreRequest $request
-     *
-     * @return RedirectResponse
-     */
-    public function store(StoreRequest $request): RedirectResponse
-    {
-        $data = $request->all(self::SPECTRUM_FIELDS);
-
-        if ($newCategory = $request->input('new_category')) {
-            $category = Category::create(['title' => $newCategory]);
-            $data['category_id'] = $category->id;
-        }
-
-        $parsedSpectrum = $this->parseSpectrum($request->file('spectrum')->getRealPath());
-
-        $data['data'] = $parsedSpectrum;
-
-        $this->getCurrentUser()->spectra()->create($data);
-
-        return redirect('home');
-    }
-
-    /**
-     * @param Spectrum $spectrum
-     *
-     * @return View
-     */
-    public function show(Spectrum $spectrum): View
-    {
-        return view('spectrum.show', [
-            'spectrum' => $spectrum->load('user', 'category'),
-        ]);
-    }
-
-    /**
-     * Get JSON representation of data
-     *
-     * @param string $path
-     *
-     * @return string
-     */
-    private function parseSpectrum(string $path): string
-    {
-        ini_set('auto_detect_line_endings', true);
-
-        $handle = fopen($path, 'rb');
-
-        $strToFloat = static function (string $str) {
-            return (float) trim(str_replace(',', '.', $str));
-        };
-
-        $points = [];
-
-        while (($data = fgetcsv($handle)) !== false) {
-            $points[] = [
-                'x' => $strToFloat($data[0]),
-                'y' => $strToFloat($data[1]),
-            ];
-        }
-
-        fclose($handle);
-
-        ini_set('auto_detect_line_endings', false);
-
-        return json_encode($points, JSON_THROW_ON_ERROR);
-    }
+//    /** @var array Spectrum model fields */
+//    private const SPECTRUM_FIELDS = [
+//        'system',
+//        'mode',
+//        'category_id',
+//        'title',
+//        'temp',
+//        'state',
+//    ];
+//
+//    /**
+//     * Show the form for creating a new resource
+//     *
+//     * @return View
+//     */
+//    public function create(): View
+//    {
+//        return view('spectrum.create', [
+//            'categories' => Category::orderBy('title')->get(),
+//        ]);
+//    }
+//
+//    /**
+//     * Store a newly created resource in storage
+//     *
+//     * @param StoreRequest $request
+//     *
+//     * @return RedirectResponse
+//     */
+//    public function store(StoreRequest $request): RedirectResponse
+//    {
+//        $data = $request->all(self::SPECTRUM_FIELDS);
+//
+//        if ($newCategory = $request->input('new_category')) {
+//            $category = Category::create(['title' => $newCategory]);
+//            $data['category_id'] = $category->id;
+//        }
+//
+//        $parsedSpectrum = $this->parseSpectrum($request->file('spectrum')->getRealPath());
+//
+//        $data['data'] = $parsedSpectrum;
+//
+//        $this->getCurrentUser()->spectra()->create($data);
+//
+//        return redirect('home');
+//    }
+//
+//    /**
+//     * @param Spectrum $spectrum
+//     *
+//     * @return View
+//     */
+//    public function show(Spectrum $spectrum): View
+//    {
+//        return view('spectrum.show', [
+//            'spectrum' => $spectrum->load('user', 'category'),
+//        ]);
+//    }
+//
+//    /**
+//     * Get JSON representation of data
+//     *
+//     * @param string $path
+//     *
+//     * @return string
+//     */
+//    private function parseSpectrum(string $path): string
+//    {
+//        ini_set('auto_detect_line_endings', true);
+//
+//        $handle = fopen($path, 'rb');
+//
+//        $strToFloat = static function (string $str) {
+//            return (float) trim(str_replace(',', '.', $str));
+//        };
+//
+//        $points = [];
+//
+//        while (($data = fgetcsv($handle)) !== false) {
+//            $points[] = [
+//                'x' => $strToFloat($data[0]),
+//                'y' => $strToFloat($data[1]),
+//            ];
+//        }
+//
+//        fclose($handle);
+//
+//        ini_set('auto_detect_line_endings', false);
+//
+//        return json_encode($points, JSON_THROW_ON_ERROR);
+//    }
 }
--- thz-ms/routes/web.php
+++ thz-ms/routes/web.php
@@ -14,23 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', static function () {
-    if (Auth::check()) {
-        return redirect('home');
-    }
+Route::get('/', 'StorageController@index')->name('home');
 
-    return view('landing');
-});
+//Auth::routes();
 
-Auth::routes();
-
-Route::middleware('auth')
-    ->group(static function () {
-        Route::get('home', 'HomeController@index')->name('home');
-        Route::get('about', 'HomeController@about')->name('about');
-        Route::resource('spectra', 'SpectrumController')->parameters([
-            'spectra' => 'spectrum',
-        ]);
-        Route::resource('categories', 'CategoryController');
-        Route::get('categories/{category}/spectra', 'CategoryController@spectra')->name('category.spectra');
-    });
+//Route::middleware('auth')
+//    ->group(static function () {
+//        Route::get('home', 'StorageController@index')->name('home');
+//    });
