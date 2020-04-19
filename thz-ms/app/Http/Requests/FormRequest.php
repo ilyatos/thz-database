28c857667eb2f2a5ad27af86846a66bcc7762e46
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-04-20 00:16:19 +0300

    Create app's ForRequest

---
+++ thz-ms/app/Http/Requests/FormRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest as BaseFormRequest;
+
+abstract class FormRequest extends BaseFormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request
+     *
+     * @return array
+     */
+    abstract public function rules(): array;
+}
