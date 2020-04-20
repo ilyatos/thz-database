8d917b80d221b48c6ed31f8ad233353cfc066078
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-04-20 23:46:35 +0300

    Create base ExperimentController with StoreExperimentRequest

---
+++ thz-ms/app/Http/Requests/Experiment/StoreExperimentRequest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Requests\Experiment;
+
+use App\Http\Requests\FormRequest;
+
+class StoreExperimentRequest extends FormRequest
+{
+    public function rules(): array
+    {
+       return [
+           'system_id' => 'required|integer',
+           'name' => 'required|alpha_dash',
+           'description' => 'present',
+       ];
+    }
+}
