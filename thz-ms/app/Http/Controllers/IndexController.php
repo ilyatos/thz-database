af3ee4f692e355b0c2b7ebb652710105c0b1ecd2
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-04-26 22:33:35 +0300

    Add IndexController to handle index requests

---
+++ thz-ms/app/Http/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+
+class IndexController extends Controller
+{
+    public function __invoke(): Response
+    {
+        return $this->response->view('app');
+    }
+}
