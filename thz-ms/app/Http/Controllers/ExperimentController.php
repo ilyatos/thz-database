f368b4ad8aaec4fbfeb5c1fa43513f4a4acde076
Ilya Goryachev <ilyatos.gor@gmail.com>
2020-05-01 22:11:19 +0300

    Add realization to 'create' and 'store' methods

--- thz-ms/app/Http/Controllers/ExperimentController.php
+++ thz-ms/app/Http/Controllers/ExperimentController.php
@@ -1,21 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Experiment\CreateExperimentRequest;
 use App\Http\Requests\Experiment\StoreExperimentRequest;
+use App\Repositories\SystemRepository;
+use App\Services\Experiment\CreateExperimentCommand;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\View\Factory;
 
 class ExperimentController extends Controller
 {
-    private Factory $view;
-
-    public function __construct(Factory $view)
-    {
-        $this->view = $view;
-    }
-
     /**
      * Display a listing of the resource.
      *
@@ -23,35 +21,46 @@ class ExperimentController extends Controller
      */
     public function index()
     {
-
     }
 
     /**
      * Show the form for creating a new resource.
      *
+     * @param CreateExperimentRequest $request
+     * @param SystemRepository        $systemRepository
+     *
      * @return Response
      */
-    public function create()
+    public function create(CreateExperimentRequest $request, SystemRepository $systemRepository): Response
     {
-        //
+        return $this->response->view(
+            'web.experiment.create',
+            [
+                'systems' => $systemRepository->getUserSystemsWithType($this->getUser()),
+                'predefinedSystemId' => (int) $request->input('system_id'),
+            ]
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  Request  $request
+     * @param StoreExperimentRequest $request
      *
-     * @return Response
+     * @return RedirectResponse
      */
-    public function store(StoreExperimentRequest $request)
+    public function store(StoreExperimentRequest $request, CreateExperimentCommand $createExperimentCommand): RedirectResponse
     {
-        //
+        $createExperimentCommand->create($this->getUser(), $request->getDto());
+
+        return $this->response->redirectToRoute('index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
     public function show($id)
@@ -62,7 +71,8 @@ class ExperimentController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
     public function edit($id)
@@ -73,8 +83,8 @@ class ExperimentController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  Request  $request
-     * @param  int  $id
+     * @param Request $request
+     * @param int     $id
      *
      * @return Response
      */
@@ -86,7 +96,8 @@ class ExperimentController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
+     *
      * @return Response
      */
     public function destroy($id)
